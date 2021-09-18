d49bc4a0b4f29803135b273d3513170c85b09a91
Braeden57 <sowinskibraeden@gmail.com>
2021-09-17 20:41:42 -0700

    1.4.4 Hotfix make sure socket.on is for self

--- LPS.js
+++ LPS.js
@@ -111,65 +111,67 @@ class Bot {
     this.socket.emit("bot_name_search", data);
     this.socket.on("bot_name_search_results", results => {
 
-      if (results.civilians.length == 0) {
-        return message.channel.send(`Name **${args[0]} ${args[1]}** not found ${message.author}`);
-      }
+      if (results.user._id==user._id) {
+        if (results.civilians.length == 0) {
+          return message.channel.send(`Name **${args[0]} ${args[1]}** not found ${message.author}`);
+        }
 
-      for (let i = 0; i < results.civilians.length; i++) {
-        // Get Drivers Licence Status
-        let licenceStatus;
-        if (results.civilians[i].civilian.licenseStatus == 1) licenceStatus = 'Valid';
-        if (results.civilians[i].civilian.licenceStatus == 2) licenceStatus = 'Revoked';
-        if (results.civilians[i].civilian.licenceStatus == 3) licenceStatus = 'None';
-        // Get Firearm Licence Status
-        let firearmLicence = results.civilians[i].civilian.firearmLicense;
-        if (firearmLicence == undefined || firearmLicence == null) firearmLicence = 'None';
-        if (firearmLicence == '2') firearmLicence = 'Valid';
-        if (firearmLicence == '3') firearmLicence = 'Revoked';
-        let nameResult = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle(`**${results.civilians[i].civilian.firstName} ${results.civilians[i].civilian.lastName} | ${results.civilians[i]._id}**`)
-        .setURL('https://discord.gg/jgUW656v2t')
-        .setAuthor('LPS Website Support', 'https://raw.githubusercontent.com/Linesmerrill/police-cad/master/lines-police-server.png', 'https://discord.gg/jgUW656v2t')
-        .setDescription('Name Search Results')
-        .addFields(
-          { name: `**First Name**`, value: `**${results.civilians[i].civilian.firstName}**`, inline: true },
-          { name: `**Last Name**`, value: `**${results.civilians[i].civilian.lastName}**`, inline: true },
-          { name: `**DOB**`, value: `**${results.civilians[i].civilian.birthday}**`, inline: true },
-          { name: `**Drivers License**`, value: `**${licenceStatus}**`, inline: true },
-          { name: `**Firearm Licence**`, value: `**${firearmLicence}**`, inline: true },
-          { name: `**Gender**`, value: `**${results.civilians[i].civilian.gender}**`, inline: true }
-        )
-        // Check Other details
-        let address = results.civilians[i].civilian.address;
-        let occupation = results.civilians[i].civilian.occupation;
-        let height = results.civilians[i].civilian.height;
-        let weight = results.civilians[i].civilian.weight;
-        let eyeColor = results.civilians[i].civilian.eyeColor;
-        let hairColor = results.civilians[i].civilian.hairColor;
-        if (address != null && address != undefined && address != '') nameResult.addFields({ name: `**Address**`, value: `**${address}**`, inline: true });
-        if (occupation != null && occupation != undefined && occupation != '') nameResult.addFields({ name: `**Occupation**`, value: `**${occupation}**`, inline: true });
-        if (height!=null&&height!=undefined&&height!="NaN"&&height!='') {
-          if (results.civilians[i].civilian.heightClassification=='imperial') {
-            let ft = Math.floor(height/12);
-            let inch = height%12;
-            nameResult.addFields({ name: '**Height**', value: `**${ft}'${inch}"**`, inline: true });
-          } else {
-            nameResult.addFields({ name: '**Height**', value: `**${height}cm**`, inline: true });
+        for (let i = 0; i < results.civilians.length; i++) {
+          // Get Drivers Licence Status
+          let licenceStatus;
+          if (results.civilians[i].civilian.licenseStatus == 1) licenceStatus = 'Valid';
+          if (results.civilians[i].civilian.licenceStatus == 2) licenceStatus = 'Revoked';
+          if (results.civilians[i].civilian.licenceStatus == 3) licenceStatus = 'None';
+          // Get Firearm Licence Status
+          let firearmLicence = results.civilians[i].civilian.firearmLicense;
+          if (firearmLicence == undefined || firearmLicence == null) firearmLicence = 'None';
+          if (firearmLicence == '2') firearmLicence = 'Valid';
+          if (firearmLicence == '3') firearmLicence = 'Revoked';
+          let nameResult = new Discord.MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(`**${results.civilians[i].civilian.firstName} ${results.civilians[i].civilian.lastName} | ${results.civilians[i]._id}**`)
+          .setURL('https://discord.gg/jgUW656v2t')
+          .setAuthor('LPS Website Support', 'https://raw.githubusercontent.com/Linesmerrill/police-cad/master/lines-police-server.png', 'https://discord.gg/jgUW656v2t')
+          .setDescription('Name Search Results')
+          .addFields(
+            { name: `**First Name**`, value: `**${results.civilians[i].civilian.firstName}**`, inline: true },
+            { name: `**Last Name**`, value: `**${results.civilians[i].civilian.lastName}**`, inline: true },
+            { name: `**DOB**`, value: `**${results.civilians[i].civilian.birthday}**`, inline: true },
+            { name: `**Drivers License**`, value: `**${licenceStatus}**`, inline: true },
+            { name: `**Firearm Licence**`, value: `**${firearmLicence}**`, inline: true },
+            { name: `**Gender**`, value: `**${results.civilians[i].civilian.gender}**`, inline: true }
+          )
+          // Check Other details
+          let address = results.civilians[i].civilian.address;
+          let occupation = results.civilians[i].civilian.occupation;
+          let height = results.civilians[i].civilian.height;
+          let weight = results.civilians[i].civilian.weight;
+          let eyeColor = results.civilians[i].civilian.eyeColor;
+          let hairColor = results.civilians[i].civilian.hairColor;
+          if (address != null && address != undefined && address != '') nameResult.addFields({ name: `**Address**`, value: `**${address}**`, inline: true });
+          if (occupation != null && occupation != undefined && occupation != '') nameResult.addFields({ name: `**Occupation**`, value: `**${occupation}**`, inline: true });
+          if (height!=null&&height!=undefined&&height!="NaN"&&height!='') {
+            if (results.civilians[i].civilian.heightClassification=='imperial') {
+              let ft = Math.floor(height/12);
+              let inch = height%12;
+              nameResult.addFields({ name: '**Height**', value: `**${ft}'${inch}"**`, inline: true });
+            } else {
+              nameResult.addFields({ name: '**Height**', value: `**${height}cm**`, inline: true });
+            }
           }
+          if (weight!=null&&weight!=undefined&&weight!='') {
+            if (results.civilians[i].civilian.weightClassification=='imperial') {
+              nameResult.addFields({ name: '**Weight**', value: `**${weight}lbs.**`, inline: true });
+            } else {
+              nameResult.addFields({ name: '**Weight**', value: `**${weight}kgs.**`, inline: true });
+            } 
+          }
+          if (eyeColor!=null&&eyeColor!=undefined&&eyeColor!='') nameResult.addFields({name:'**Eye Color**',value:`**${eyeColor}**`,inline:true});
+          if (hairColor!=null&&hairColor!=undefined&&hairColor!='') nameResult.addFields({name:'**Hair Color**',value:`**${hairColor}**`,inline:true});
+          nameResult.addFields({name:'**Organ Donor**',value:`**${results.civilians[i].civilian.organDonor}**`,inline:true});
+          nameResult.addFields({name:'**Veteran**',value:`**${results.civilians[i].civilian.veteran}**`,inline:true});
+          message.channel.send(nameResult);
         }
-        if (weight!=null&&weight!=undefined&&weight!='') {
-          if (results.civilians[i].civilian.weightClassification=='imperial') {
-            nameResult.addFields({ name: '**Weight**', value: `**${weight}lbs.**`, inline: true });
-          } else {
-            nameResult.addFields({ name: '**Weight**', value: `**${weight}kgs.**`, inline: true });
-          } 
-        }
-        if (eyeColor!=null&&eyeColor!=undefined&&eyeColor!='') nameResult.addFields({name:'**Eye Color**',value:`**${eyeColor}**`,inline:true});
-        if (hairColor!=null&&hairColor!=undefined&&hairColor!='') nameResult.addFields({name:'**Hair Color**',value:`**${hairColor}**`,inline:true});
-        nameResult.addFields({name:'**Organ Donor**',value:`**${results.civilians[i].civilian.organDonor}**`,inline:true});
-        nameResult.addFields({name:'**Veteran**',value:`**${results.civilians[i].civilian.veteran}**`,inline:true});
-        message.channel.send(nameResult);
       }
     });
   }
@@ -188,38 +190,54 @@ class Bot {
     this.socket.emit('bot_plate_search', data);
     this.socket.on('bot_plate_search_results', results => {
       
-      if (results.vehicles.length == 0) {
-        return message.channel.send(`Plate Number **${args[0]}** not found ${message.author}`);
-      }
+      if (results.user._id==user._id) {
+        if (results.vehicles.length == 0) {
+          return message.channel.send(`Plate Number **${args[0]}** not found ${message.author}`);
+        }
 
-      for (let i = 0; i < results.vehicles.length; i++) {
-        let plateResult = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-        .setTitle(`**${results.vehicles[i].vehicle.plate} | ${results.vehicles[i]._id}**`)
-        .setURL('https://discord.gg/jgUW656v2t')
-        .setAuthor('LPS Website Support', 'https://raw.githubusercontent.com/Linesmerrill/police-cad/master/lines-police-server.png', 'https://discord.gg/jgUW656v2t')
-        .setDescription('Plate Search Results')
-        .addFields(
-          { name: `**Plate #**`, value: `**${results.vehicles[i].vehicle.plate}**`, inline: true },
-          { name: `**Vin #**`, value: `**${results.vehicles[i].vehicle.vin}**`, inline: true },
-          { name: `**Model**`, value: `**${results.vehicles[i].vehicle.model}**`, inline: true },
-          { name: `**Color**`, value: `**${results.vehicles[i].vehicle.color}**`, inline: true },
-          { name: `**Owner**`, value: `**${results.vehicles[i].vehicle.registeredOwner}**`, inline: true },
-        )
-        // Other details
-        let validRegistration = results.vehicles[i].vehicle.validRegistration;
-        let validInsurance = results.vehicles[i].vehicle.validInsurance;
-        let stolen = results.vehicles[i].vehicle.isStolen;
-        if (validRegistration=='1') plateResult.addFields({ name: `**Registration**`, value: `**Valid**`, inline: true });
-        if (validRegistration=='2') plateResult.addFields({ name: `**Registration**`, value: `**InValid**`, inline: true });
-        if (validInsurance=='1') plateResult.addFields({ name: `**Insurance**`, value: `**Valid**`, inline: true });
-        if (validInsurance=='2') plateResult.addFields({ name: `**Insurance**`, value: `**InValid**`, inline: true });
-        if (stolen=='1') plateResult.addFields({ name: `**Stolen**`, value: `**No**`, inline: true });
-        if (stolen=='2') plateResult.addFields({ name: `**Stolen**`, value: `**Yes**`, inline: true });
-        message.channel.send(plateResult);
+        for (let i = 0; i < results.vehicles.length; i++) {
+          let plateResult = new Discord.MessageEmbed()
+          .setColor('#0099ff')
+          .setTitle(`**${results.vehicles[i].vehicle.plate} | ${results.vehicles[i]._id}**`)
+          .setURL('https://discord.gg/jgUW656v2t')
+          .setAuthor('LPS Website Support', 'https://raw.githubusercontent.com/Linesmerrill/police-cad/master/lines-police-server.png', 'https://discord.gg/jgUW656v2t')
+          .setDescription('Plate Search Results')
+          .addFields(
+            { name: `**Plate #**`, value: `**${results.vehicles[i].vehicle.plate}**`, inline: true },
+            { name: `**Vin #**`, value: `**${results.vehicles[i].vehicle.vin}**`, inline: true },
+            { name: `**Model**`, value: `**${results.vehicles[i].vehicle.model}**`, inline: true },
+            { name: `**Color**`, value: `**${results.vehicles[i].vehicle.color}**`, inline: true },
+            { name: `**Owner**`, value: `**${results.vehicles[i].vehicle.registeredOwner}**`, inline: true },
+          )
+          // Other details
+          let validRegistration = results.vehicles[i].vehicle.validRegistration;
+          let validInsurance = results.vehicles[i].vehicle.validInsurance;
+          let stolen = results.vehicles[i].vehicle.isStolen;
+          if (validRegistration=='1') plateResult.addFields({ name: `**Registration**`, value: `**Valid**`, inline: true });
+          if (validRegistration=='2') plateResult.addFields({ name: `**Registration**`, value: `**InValid**`, inline: true });
+          if (validInsurance=='1') plateResult.addFields({ name: `**Insurance**`, value: `**Valid**`, inline: true });
+          if (validInsurance=='2') plateResult.addFields({ name: `**Insurance**`, value: `**InValid**`, inline: true });
+          if (stolen=='1') plateResult.addFields({ name: `**Stolen**`, value: `**No**`, inline: true });
+          if (stolen=='2') plateResult.addFields({ name: `**Stolen**`, value: `**Yes**`, inline: true });
+          message.channel.send(plateResult);
+        }
       }
     });
   }
+
+  // In Dev
+  // async firearmSeach(message, args) {
+  //   let user = await this.dbo.collection("users").findOne({"user.discord.id":message.author.id}).then(user => user);
+  //   if (!user) return message.channel.send(`You are not logged in ${message.author}!`);
+  //   if (args.length==0) return message.channel.send(`You are missing a **Serial Number** ${message.author}`);
+  //   let data = {
+  //     user: user,
+  //     query: {
+  //       serialNumber: args[0],
+  //       activeCommunityID: user.user.activeCommunity
+  //     }
+  //   }
+  // }
 
   async checkStatus(message, args) {
     if (args.length==0) {
