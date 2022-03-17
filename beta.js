50fcd6900c78389a246a19d247c24f999bcde1ba
Braeden Sowinski <sowinskibraeden@gmail.com>
2022-03-17 16:10:23 -0700

    clean

--- beta.js
+++ beta.js
@@ -2,5 +2,5 @@ const LinesPoliceCadBot = require('./structures/LinesPoliceCadBot');
 const config = require('./config/beta-config');
 const { Intents } = require('discord.js');
 
-let client =  new LinesPoliceCadBot({ intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]}, config);
+let client = new LinesPoliceCadBot({ intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]}, config);
 client.build()
