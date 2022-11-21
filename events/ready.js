cb118a886facb6cd1446ec86d264001c214ba8c9
SowinskiBraeden <sowinskibraeden@gmail.com>
2022-11-21 14:21:42 -0800

    fix bot activity

--- events/ready.js
+++ events/ready.js
@@ -1,11 +1,9 @@
 module.exports = async (client) => {
   (client.Ready = true),
-    client.user.setActivity(client.config.Presence.name, {
-      type: client.config.Presence.type,
-    })
-    client.user.setPresence({
-      status: client.config.Presence.status, // You can show online, idle, and dnd
-    });
+    client.user.setActivity(
+      client.config.Presence.name, 
+      client.config.Presence.type
+    )
   client.log(`Successfully Logged in as ${client.user.tag}`); // You can change the text if you want, but DO NOT REMOVE "client.user.tag"
   client.log(`Ready to serve in ${client.channels.cache.size} channels on ${client.guilds.cache.size} servers, for a total of ${client.users.cache.size} users.`)
   client.RegisterSlashCommands();
