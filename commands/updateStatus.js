83aca4b7b0a69f298f878c8d9068ed8d79463b59
Braeden Sowinski <sowinskibraeden@gmail.com>
2023-06-10 15:27:37 -0700

    Debug updateStatus not logged in check

--- commands/updateStatus.js
+++ commands/updateStatus.js
@@ -41,7 +41,7 @@ module.exports = {
       if (!useCommand) return interaction.send({ content: "You don't have permission to use this command" });
 
       let user = await client.dbo.collection("users").findOne({"user.discord.id":interaction.member.user.id}).then(user => user);
-      if (!user) return interaction.send({ content: `You are not logged in <@${interactoin.member.user.id}>` });
+      if (!user) return interaction.send({ content: `You are not logged in <@${interaction.member.user.id}>` });
       if (user.user.activeCommunity==null) return interaction.send({ content: `You must join a community to use this command.` });
       let onDuty=null;
       let updateDuty=false;
