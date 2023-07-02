21e1eec5144b1b2c35dc82a8fc50a4fe72be8a5c
Braeden Sowinski <sowinskibraeden@gmail.com>
2023-07-01 18:51:57 -0700

    reset channels

---
+++ commands/reset-channels.js
@@ -0,0 +1,26 @@
+const { EmbedBuilder } = require('discord.js');
+
+module.exports = {
+  name: "setchannel",
+  description: "Add channel to allowed channels",
+  usage: "[channel]",
+  permissions: {
+    channel: ["VIEW_CHANNEL", "SEND_MESSAGES", "EMBED_LINKS"],
+    member: ["MANAGE_GUILD"],
+  },  
+  SlashCommand: {
+    /**
+     *
+     * @param {require("../structures/LinesPoliceCadBot")} client
+     * @param {import("discord.js").MessageCreate} message
+     * @param {string[]} args
+     * @param {*} param3
+    */
+    run: async (client, interaction, args, { GuildDB }) => {
+      client.dbo.collection("prefixes").updateOne({"server.serverID":interaction.guild.id},{set:{"server.allowedChannels":[]]},$set:{"server.hasCustomChannels":false}},function(err, res) {
+        if (err) throw err;
+        return interaction.send({ content: `Successfully reset channels..` });
+      });
+    },
+  },
+}
